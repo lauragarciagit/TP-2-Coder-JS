03fbd89405bd0f01f119eb92cf0eb76766357163
Laura Fernanda Garcia <lauragarcia.cv@gmail.com>
2025-06-04 13:15:57 -0300

    ej3 del tp2

--- script2.js
+++ script2.js
@@ -1,5 +1,4 @@
 
-
 let numeroingresado = Number(prompt("Ingrese un número"));
 
 alert("El número que ingresaste es: " + (numeroingresado));
@@ -9,6 +8,10 @@ let numeroasumar = Number(prompt("Ingrese otro número"))
 alert("El número sumado es: " + (numeroingresado + numeroasumar))
 
 
+let i = (prompt("Ingrese un número"))
+for (let i = 0; i < 3; i++) {
+    console.log("Hola" + i);
+}
 
 
 
