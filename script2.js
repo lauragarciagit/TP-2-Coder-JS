bae39166463961eba9cc6febfe0fa443c865db70
Laura Fernanda Garcia <lauragarcia.cv@gmail.com>
2025-06-04 12:40:37 -0300

    ingreso prompt

---
+++ script2.js
@@ -0,0 +1,14 @@
+
+
+let numeroingresado = Number(prompt("Ingrese un número"));
+
+alert("El número que ingresaste es: " + (numeroingresado));
+
+let numeroasumar = numeroingresado + numeroasumar
+
+alert("El número sumado es: " + (numeroingresado + numeroasumar))
+
+
+
+
+
