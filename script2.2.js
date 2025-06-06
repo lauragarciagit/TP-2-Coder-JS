04a722ccfa32d9c1e5bfe28563ff799637227adc
Laura Fernanda Garcia <lauragarcia.cv@gmail.com>
2025-06-06 11:45:24 -0300

    Deje solo el ejercicio 1

--- script2.2.js
+++
@@ -1,2 +0,0 @@
-let nombre = prompt("Ingrese su nombre");
-console.log("Su nombre es: " + nombre);
--- script2.3.js
+++
@@ -1,5 +0,0 @@
-
-let i = (prompt("Ingrese un número"))
-for (let i = 0; i < 3; i++) {
-    console.log("Hola" + i);
-}
